ce5d90342c211baf894cf93f56fa4bc68bfa2c0e
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-11-25 15:06:23 -0500

    styles for home page

---
+++ src/pages/home/style.js
@@ -0,0 +1,10 @@
+import background from "../../assets/img/background.jpg";
+
+const styleHome = {
+  maxHeight: "100vh",
+  height: "100vh",
+  backgroundImage: `url(${background})`,
+  backgroundSize: "cover",
+};
+
+export { styleHome };
