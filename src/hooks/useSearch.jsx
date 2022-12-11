4b3b092025b40b8d6d02b7fe961cce32a93d031b
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-10 21:56:18 -0500

    use of onChange event

--- src/hooks/useSearch.jsx
+++ src/hooks/useSearch.jsx
@@ -1,19 +1,16 @@
-import { useRef } from "react";
-import { findDogs } from "../redux/slices/dogsSlice";
-import { useDispatch } from "react-redux";
+import { findDogs } from '../redux/slices/dogsSlice';
+import { useDispatch } from 'react-redux';
 
 const useSearch = () => {
-  const inputSearchRef = useRef();
   const dispatch = useDispatch();
 
-  const handleSearch = () => {
-    const value = inputSearchRef.current.value;
+  const handleSearch = (e) => {
+    const value = e.target.value;
     dispatch(findDogs(value));
   };
 
   return {
     handleSearch,
-    inputSearchRef,
   };
 };
 
