893d00e986a5421c45091f42de8694e897803738
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-10 22:51:37 -0500

    modifications for pag component

--- src/components/pag/index.jsx
+++ src/components/pag/index.jsx
@@ -1,14 +1,24 @@
-import { Pagination } from "@mui/material";
-import usePage from "../../hooks/usePage";
+import { Pagination, Grid } from '@mui/material';
+import usePage from '../../hooks/usePage';
 
 const Pag = () => {
   const { handleChangePage, handleSizePage, handlePageNumber } = usePage();
+
+  if (handleSizePage() === 1) return;
+
   return (
-    <Pagination
-      count={handleSizePage()}
-      defaultPage={handlePageNumber()}
-      onChange={handleChangePage}
-    />
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+    >
+      <Pagination
+        count={handleSizePage()}
+        defaultPage={handlePageNumber()}
+        onChange={handleChangePage}
+      />
+    </Grid>
   );
 };
 
