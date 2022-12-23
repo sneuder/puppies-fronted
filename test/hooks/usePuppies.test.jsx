7aaa2e843d83824a3b2949fbe3729e36a254c500
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-23 10:50:58 -0500

    modifications & new testing package

--- test/hooks/usePuppies.test.jsx
+++ test/hooks/usePuppies.test.jsx
@@ -1,32 +1,25 @@
 import '@testing-library/jest-dom';
 import { render, waitFor } from '@testing-library/react';
-import { renderHook } from '@testing-library/react';
-// import { renderHook } from '@testing-library/react-hooks';
+// import { renderHook } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks';
 import usePuppies from '../../src/hooks/usePuppies';
 
 import { Provider } from 'react-redux';
 import store from '../../src/redux/store';
 
 import { MemoryRouter } from 'react-router-dom';
-import App from '../../src/App';
 
 describe('usePuppies custome hook', () => {
   it('should have 10 dogs by default', async () => {
-    const { result } = renderHook(() => usePuppies(), {
-      wrapper: () => (
+    const { waitForNextUpdate, result } = renderHook(() => usePuppies(), {
+      wrapper: ({ children }) => (
         <Provider store={store}>
-          <MemoryRouter initialEntries={['/dogs']}>
-            <App />
-          </MemoryRouter>
+          <MemoryRouter initialEntries={['/dogs']}>{children}</MemoryRouter>
         </Provider>
       ),
     });
 
-    // await waitForNextUpdate();
-    await waitFor(() => {
-      console.log(result.current);
-      // const dogsLength = result.current.rows.length;
-      // expect(dogsLength).toHaveProperty('rows');
-    });
+    await waitForNextUpdate();
+    expect(result.current.dogs.length).toBe(10);
   });
 });
