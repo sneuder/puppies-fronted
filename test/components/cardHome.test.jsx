fdde0184803f271de696f994fd7f9c6fedb92588
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-22 22:36:54 -0500

    new devDependencies and tests

--- test/components/cardHome.test.jsx
+++ test/components/cardHome.test.jsx
@@ -1,17 +1,39 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
 import '@testing-library/jest-dom';
 
+import { render, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter, BrowserRouter } from 'react-router-dom';
+
+import { Provider } from 'react-redux';
+import store from '../../src/redux/store';
+
 import CardHome from '../../src/components/cardHome';
+import App from '../../src/App';
 
 describe('CardHome component', () => {
   it('should render CardHome component', () => {
     render(<CardHome />, { wrapper: MemoryRouter });
   });
 
-  it('should have a button', () => {
-    render(<CardHome />, { wrapper: MemoryRouter });
-    const element = screen.getByText('Check more Dogs!');
-    expect(element).toBeInTheDocument();
+  it('should have a link to /dogs', () => {
+    const { container } = render(<CardHome />, { wrapper: MemoryRouter });
+    const element = container.querySelector('a');
+    expect(element).toHaveAttribute('href', '/dogs');
+  });
+
+  it('should able to take me to /dogs', async () => {
+    const { container } = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </Provider>
+    );
+    const element = container.querySelector('a');
+    fireEvent.click(element);
+
+    await waitFor(() => {
+      const pathname = window.location.pathname;
+      expect(pathname).toBe('/dogs');
+    });
   });
 });
