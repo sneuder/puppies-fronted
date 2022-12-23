35195f4a6600dbe710a932b654e2fd803b5877e6
Esneider Ospina Oviedo <sneuder8@gmail.com>
2022-12-23 09:16:27 -0500

    modifications on tests

--- test/components/cardHome.test.jsx
+++ test/components/cardHome.test.jsx
@@ -1,33 +1,34 @@
 import '@testing-library/jest-dom';
 
 import { render, fireEvent, waitFor } from '@testing-library/react';
-import { MemoryRouter, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 import store from '../../src/redux/store';
 
-import CardHome from '../../src/components/cardHome';
 import App from '../../src/App';
 
 describe('CardHome component', () => {
-  it('should render CardHome component', () => {
-    render(<CardHome />, { wrapper: MemoryRouter });
-  });
+  let appComponent;
 
-  it('should have a link to /dogs', () => {
-    const { container } = render(<CardHome />, { wrapper: MemoryRouter });
-    const element = container.querySelector('a');
-    expect(element).toHaveAttribute('href', '/dogs');
-  });
-
-  it('should able to take me to /dogs', async () => {
-    const { container } = render(
+  beforeEach(() => {
+    appComponent = render(
       <Provider store={store}>
         <BrowserRouter>
           <App />
         </BrowserRouter>
       </Provider>
     );
+  });
+
+  it('should have a link to /dogs', () => {
+    const { container } = appComponent;
+    const element = container.querySelector('a');
+    expect(element).toHaveAttribute('href', '/dogs');
+  });
+
+  it('should able to take me to /dogs', async () => {
+    const { container } = appComponent;
     const element = container.querySelector('a');
     fireEvent.click(element);
 
